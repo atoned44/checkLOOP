8733619d2be16092eb7ef760b43b3e79624fd193
AtoneAnnie <68375146+atoned44@users.noreply.github.com>
2024-12-13 23:21:29 +0000

    Update main.py
    Adding comments at the end of

--- main.py
+++ main.py
@@ -39,5 +39,7 @@ def send_notification():
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
 
+<!-- 
 if __name__ == "__main__":
-    send_notification()
+    send_notification() 
+-->
